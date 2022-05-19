f6cb44ea748afae9d09bcb5a3f541af404813abd
mendbaltraz <mendbaltraz@gmail.com>
2022-05-19 10:53:08 +0100

    change

---
+++ discord/commands/message/restart.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+module.exports = {
+  name: "restart",
+  devOnly: true,
+  alias: ["rs"],
+  async execute(message, args, client) {
+    await message.channel.send("Restarting ...");
+    exec("pm2 restart 0", (err, stdout, stderr) => {
+      console.log({ err, stdout, stderr });
+    });
+  },
+};
